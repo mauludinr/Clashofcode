dff7bcee471816cd8949dc6a50456c19f1952774
mauludinr <38213112+mauludinr@users.noreply.github.com>
2021-04-12 13:42:19 +0700

    Update Case3.py

--- Case3.py
+++ Case3.py
@@ -3,14 +3,18 @@
 
 # Example:
 # If time is 30 and speed is 20, you need to output 10.
+
 # Input
 # Line 1: An integer time for the time Johny was eating sugar.
 # Line 2: An integer speed for the speed he ate at in grams per minute.
+
 # Output
 # Line 1: The amount in grams.
+
 # Constraints
 # 1 ≤ time ≤ 9^9
 # 1 ≤ speed ≤ 9^9
+
 # Example
 # Input
 # 30
