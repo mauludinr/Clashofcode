50c2ff232928f7a623b7705d23f83e6ba27af2e5
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-09-15 19:18:08 +0700

    Create Case3.py

---
+++ Case3.py
@@ -0,0 +1,8 @@
+You are given a string w, and a single character c.
+c is equal to either S or D.
+
+You need to print the word w as many times as the length of w. You print w once and then print w reversed, you print w once and then print w reversed and so on...
+
+if c equal to S you need to have spaces between each word.
+
+if c equal to D you need to have line breaks between each word.
