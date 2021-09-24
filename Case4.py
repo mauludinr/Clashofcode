e1b5e45f13a41b5cffa655b4a77959062b71aa19
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-09-24 20:16:41 +0700

    Update Case4.py

--- Case4.py
+++ Case4.py
@@ -1,24 +1,27 @@
 # The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
-# 01 Test 1
+# 1) Test 1
 # Input
-# Expected output
 # Hello there!!
+# Expected output
 # there!! Hello
-# 02 Test 2
+
+# 2) Test 2
 # Input
-# Expected output
 # We had a three-course meal.
+# Expected output
 # meal. three-course a had We
-# 03 Test 3
+
+# 3) Test 3
 # Input
-# Expected output
 # Oh, how I'd love to go!
-# go! to love I'd how Oh,
-# 04 Test 4
-# Input
 # Expected output
+# go! to love I'd how Oh,
+
+# 4) Test 4
+# Input
 # Reaching the large house near the Horse Guards' barracks, in which Anatole lived, Pierre entered the lighted porch, ascended the stairs, and went in at the open door.
-# door. open the at in went and stairs,
+# Expected output
+# door. open the at in went and stairs, the ascended porch, lighted the entered Pierre lived, Anatole which in barracks, Guards' Horse the near house large the Reaching
 
 s = input()
 z = s.split(sep=" ")
