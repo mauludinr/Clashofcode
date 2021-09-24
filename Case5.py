bf4ba0fcca18cbccf575e54be88520ce7964d309
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-09-24 20:08:06 +0700

    Create Case5.py

---
+++ Case5.py
@@ -0,0 +1,15 @@
+Goal
+Given a string, output the sum of each ASCII character value multiplied by its index in the string
+Input
+Line 1: A string S
+Output
+Line 1: The sum of each ASCII character value multiplied by its index.
+
+Example: ABC => 65*0 + 66*1 + 67*2 => 200
+Constraints
+The string contains no space.
+Example
+Input
+ABC
+Output
+200
